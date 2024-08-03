adae772d99a185d813ec7389446426c4e23369e8
Takeda04 <olimjonnishanaliyev0804@gmail.com>
2024-08-03 17:37:04 +0500

    Changed for builds and for fixing bugs

--- app/index.tsx
+++ app/index.tsx
@@ -31,6 +31,7 @@ export default function Index() {
         setDoctors(doctorsList);
       } catch (error) {
         setError(error.message);
+        console.error('Error fetching doctors:', error);
       } finally {
         setLoading(false);
       }
@@ -38,6 +39,8 @@ export default function Index() {
 
     fetchDoctors();
   }, [router]);
+
+  console.log(doctors)
 
   if (loading) {
     return <Start />;
